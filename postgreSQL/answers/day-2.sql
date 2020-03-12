f0923c823687945e35f877c980a96e266cb888c2
vladimir <vladimir.zhuravlev@yalantis.com>
2020-03-12 14:38:55 +0200

    Add answers to day 2

---
+++ postgreSQL/answers/day-2.sql
@@ -0,0 +1,24 @@
+-- 1) ==========================================================================
+CREATE RULE delete_venue AS
+  ON DELETE TO venues DO INSTEAD
+  UPDATE venues SET active = false WHERE venue_id = OLD.venue_id;
+-- 2) ==========================================================================
+CREATE EXTENSION tablefunc;
+SELECT * FROM crosstab(
+    'SELECT extract(year from starts) as year,
+  extract(month from starts) as month, count(*)
+  FROM events
+  GROUP BY year, month',
+  'SELECT * FROM generate_series(1, 12);'
+) AS (
+  year int,
+  jan int, feb int, mar int, apr int, may int, jun int,
+  jul int, aug int, sep int, oct int, nov int, dec int
+) ORDER BY YEAR;
+-- 3) ==========================================================================
+SELECT * FROM crosstab(
+    'SELECT extract(week from starts) AS week, extract(dow from starts) AS dow, count(*) FROM events GROUP BY week, dow ORDER BY week, dow;',
+    'SELECT * FROM generate_series(0,6)'
+) AS (
+    week int, Sun int, Mon int, Tue int, Wed int, Thu int, Fri int, Sat int
+) ORDER BY week;
